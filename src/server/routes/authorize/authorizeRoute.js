b3498c8c6166bc08a82f90b9a00518e1912290c1
Mateusz Suder <matisu999@gmail.com>
2022-12-07 22:54:59 +0100

    Remove authorization

--- src/server/routes/authorize/authorizeRoute.js
+++ src/server/routes/authorize/authorizeRoute.js
@@ -1,9 +1,8 @@
 import express from "express";
 import authorize from "../../../services/authorize/authorize.js";
-import withAuth from "../../../framework/middlewares/withAuth.js";
 
 const router = express.Router();
 
-router.post("/", withAuth({ role: "ADMIN" }), authorize);
+router.post("/", authorize);
 
 export default router;
