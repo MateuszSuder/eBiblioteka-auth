0dd55138ea814cffe343a158211cf5d4b91382c1
Mateusz Suder <matisu999@gmail.com>
2022-12-07 22:29:35 +0100

    Add admin to authorize

--- src/server/routes/authorize/authorizeRoute.js
+++ src/server/routes/authorize/authorizeRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
 import authorize from "../../../services/authorize/authorize.js";
+import withAuth from "../../../framework/middlewares/withAuth.js";
 
 const router = express.Router();
 
-router.post("/", authorize);
+router.post("/", withAuth({ role: "ADMIN" }), authorize);
 
 export default router;
